7591eba4f2da9bdeecc449b50398f024739c489c
Theo Bob Massard <tbobm@protonmail.com>
2024-12-18 15:25:26 +0100

    build(utils): add localstack support
    Signed-off-by: Theo Bob Massard <tbobm@protonmail.com>

---
+++ init-localstack.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+DLQ_SQS=input-queue-dlq
+SOURCE_SQS=input-queue
+
+QUEUE_URL=$(awslocal sqs --region eu-west-1 create-queue --queue-name $DLQ_SQS | grep '"QueueUrl"' | awk -F '"QueueUrl":' '{print $2}' | tr -d '"' | xargs)
+
+DLQ_SQS_ARN=$(awslocal sqs --region eu-west-1 get-queue-attributes \
+                  --attribute-name QueueArn --queue-url=$QUEUE_URL \
+                  |  sed 's/"QueueArn"/\n"QueueArn"/g' | grep '"QueueArn"' | awk -F '"QueueArn":' '{print $2}' | tr -d '"' | xargs)
+
+awslocal sqs --region eu-west-1 create-queue --queue-name $SOURCE_SQS \
+     --attributes '{
+                   "RedrivePolicy": "{\"deadLetterTargetArn\":\"'"$DLQ_SQS_ARN"'\",\"maxReceiveCount\":\"2\"}",
+                   "VisibilityTimeout": "20"
+                   }'
+
+
+DLQ_SQS=result-queue-dlq
+SOURCE_SQS=result-queue
+
+QUEUE_URL=$(awslocal sqs --region eu-west-1 create-queue --queue-name $DLQ_SQS | grep '"QueueUrl"' | awk -F '"QueueUrl":' '{print $2}' | tr -d '"' | xargs)
+
+DLQ_SQS_ARN=$(awslocal sqs --region eu-west-1 get-queue-attributes \
+                  --attribute-name QueueArn --queue-url=$QUEUE_URL \
+                  |  sed 's/"QueueArn"/\n"QueueArn"/g' | grep '"QueueArn"' | awk -F '"QueueArn":' '{print $2}' | tr -d '"' | xargs)
+
+awslocal sqs --region eu-west-1 create-queue --queue-name $SOURCE_SQS \
+     --attributes '{
+                   "RedrivePolicy": "{\"deadLetterTargetArn\":\"'"$DLQ_SQS_ARN"'\",\"maxReceiveCount\":\"2\"}",
+                   "VisibilityTimeout": "20"
+                   }'
+
