3f42223167092def7de5e4301136ae5e416a943a
YannPl <yann.plantevin@meero.com>
2025-01-14 07:58:31 +0100

    feat(config):  load env vars into declared config keys

--- cmd/main.go
+++ cmd/main.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
-	"github.com/meero-com/guild-proxy/pkg/api"
 	"github.com/meero-com/guild-proxy/config"
+	"github.com/meero-com/guild-proxy/pkg/api"
 )
 
 func main() {
--- config/config.go
+++ config/config.go
@@ -1,33 +1,49 @@
 package config
 
 import (
-    "log"
+	"log"
+	"strings"
 
 	"github.com/spf13/pflag"
-    "github.com/spf13/viper"
+	"github.com/spf13/viper"
 )
 
 func InitConfig() {
-	// Define command-line flags
-    pflag.String("env", "development", "Application environment")
-    pflag.Int("server.port", 8080, "Server port")
+	viper.SetConfigName("config")
+	viper.AddConfigPath("config/")
+	viper.SetConfigType("yaml")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
-    // Parse command-line flags
-    pflag.Parse()
+	// Read the configuration file
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file, %s", err)
+	}
 
-    // Bind flags to Viper
-    viper.BindPFlags(pflag.CommandLine)
-
-    viper.SetConfigName("config")
-    viper.AddConfigPath("config/")
-    viper.SetConfigType("yaml")
-
-    // Read the configuration file
-    if err := viper.ReadInConfig(); err != nil {
-        log.Fatalf("Error reading config file, %s", err)
-    }
+	// Bind each key in the config file to a command-line flag
+	for _, key := range viper.AllKeys() {
+		switch viper.Get(key).(type) {
+		case int:
+			pflag.Int(key, viper.GetInt(key), "")
+		case bool:
+			pflag.Bool(key, viper.GetBool(key), "")
+		case float64:
+			pflag.Float64(key, viper.GetFloat64(key), "")
+		case string:
+			pflag.String(key, viper.GetString(key), "")
+		default:
+			pflag.String(key, viper.GetString(key), "")
+		}
+	}
+	// Parse and bind command-line flags
+	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
 }
 
 func GetConfig(key string) interface{} {
-    return viper.Get(key)
+	return viper.Get(key)
+}
+
+func GetAllConfig() map[string]interface{} {
+	return viper.AllSettings()
 }
